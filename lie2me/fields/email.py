bd92a5d0e0680f34586bb35afe21405bbb1c6480
Hugo Mota <hugo.txt@gmail.com>
2017-09-15 09:54:57 -0300

    Update email field to use the pattern constraint of text

--- lie2me/fields/email.py
+++ lie2me/fields/email.py
@@ -1,17 +1,11 @@
-import re
 from .text import Text
 
 
 class Email(Text):
 
     max = 254
+    pattern = r'^[^@]+@[^@]+\.[^@]+$'
 
     messages = {
-        'type': 'Not a valid email address',
+        'pattern': 'Not a valid email address',
     }
-
-    def validation(self, value):
-        value = super(Email, self).validation(value)
-        if not re.match(r'^[^@]+@[^@]+\.[^@]+$', value):
-            raise self.error('type')
-        return value
