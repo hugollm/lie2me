5367537f4b80d153cdef8e41a24decf81662d53c
Hugo Mota <hugo.txt@gmail.com>
2017-09-20 19:08:39 -0300

    Update time parsers to return None instead of raising on invalid values

--- lie2me/parsers.py
+++ lie2me/parsers.py
@@ -3,6 +3,7 @@ import re
 
 
 def parse_time(string):
+    string = str(string).strip()
     pattern = ''.join([
         r'^([0-2][0-9])', # h
         r'(:([0-5][0-9]))?', # m
@@ -12,29 +13,35 @@ def parse_time(string):
     ])
     match = re.match(pattern, string)
     if match is None:
-        raise ValueError('Invalid time')
+        return None
     h, _, m, _, s, _, ms, z = match.groups()
     h = int(h)
     m = int(m) if m is not None else 0
     s = int(s) if s is not None else 0
-    ms = int(ms) * 1000 if ms is not None else 0
-    tm = time(h, m, s, ms)
+    mcs = int(ms) * 1000 if ms is not None else 0
+    try:
+        tm = time(h, m, s, mcs)
+    except ValueError:
+        return None
     if z is not None:
         tz = parse_timezone(z)
+        if tz is None:
+            return None
         tm = tm.replace(tzinfo=timezone(tz))
     return tm
 
 
 def parse_timezone(string):
+    string = str(string).strip()
     pattern = r'^(Z)|([+-])([0-2][0-9])(:?([0-5][0-9]))?$'
     match = re.match(pattern, string)
     if match is None:
-        raise ValueError('Invalid timezone')
+        return None
     z, signal, h, _, m = match.groups()
     h = int(signal + h) if h is not None else 0
     m = int(signal + m) if m is not None else 0
     if h == 0 and signal == '-':
-        raise ValueError('Invalid timezone')
+        return None
     if abs((h * 60) + m) > (24 * 60) - 1:
-        raise ValueError('Invalid timezone')
+        return None
     return timedelta(hours=h, minutes=m)
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -5,10 +5,6 @@ from lie2me.parsers import parse_time, parse_timezone
 
 class TimeParserTestCase(TestCase):
 
-    def aware_time(self, hour, minute=0, second=0, microsecond=0, offset=0):
-        tm = time(hour, minute, second, microsecond)
-        return tm.replace(tzinfo=timezone(timedelta(hours=offset)))
-
     def test_valid_naive_times(self):
         self.assertEqual(parse_time('08:30:59'), time(8, 30, 59))
         self.assertEqual(parse_time('00:00:00'), time(0))
@@ -16,31 +12,22 @@ class TimeParserTestCase(TestCase):
         self.assertEqual(parse_time('08'), time(8))
         self.assertEqual(parse_time('08:30.999'), time(8, 30, 0, 999000))
 
+    def test_invalid_inputs(self):
+        self.assertEqual(parse_time(None), None)
+        self.assertEqual(parse_time([]), None)
+        self.assertEqual(parse_time(''), None)
+
     def test_invalid_naive_times(self):
-        with self.assertRaises(TypeError):
-            parse_time(None)
-        with self.assertRaises(ValueError):
-            parse_time('')
-        with self.assertRaises(ValueError):
-            parse_time('8')
-        with self.assertRaises(ValueError):
-            parse_time('24')
-        with self.assertRaises(ValueError):
-            parse_time('08:')
-        with self.assertRaises(ValueError):
-            parse_time('08:0')
-        with self.assertRaises(ValueError):
-            parse_time('23:60')
-        with self.assertRaises(ValueError):
-            parse_time('23-60')
-        with self.assertRaises(ValueError):
-            parse_time('08:00:0')
-        with self.assertRaises(ValueError):
-            parse_time('08:00:60')
-        with self.assertRaises(ValueError):
-            parse_time('08:00:59.')
-        with self.assertRaises(ValueError):
-            parse_time('08:00:59.9999')
+        self.assertEqual(parse_time('8'), None)
+        self.assertEqual(parse_time('24'), None)
+        self.assertEqual(parse_time('08:'), None)
+        self.assertEqual(parse_time('08:0'), None)
+        self.assertEqual(parse_time('23:60'), None)
+        self.assertEqual(parse_time('23-60'), None)
+        self.assertEqual(parse_time('08:00:0'), None)
+        self.assertEqual(parse_time('08:00:60'), None)
+        self.assertEqual(parse_time('08:00:59.'), None)
+        self.assertEqual(parse_time('08:00:59.9999'), None)
 
     def test_valid_aware_times(self):
         self.assertEqual(parse_time('08Z'), self.aware_time(8, offset=0))
@@ -67,19 +54,17 @@ class TimeParserTestCase(TestCase):
         self.assertEqual(parse_time('08:30:59+03'), self.aware_time(8, 30, 59, offset=3))
         self.assertEqual(parse_time('08:30:59+23:00'), self.aware_time(8, 30, 59, offset=23))
 
+    def aware_time(self, hour, minute=0, second=0, microsecond=0, offset=0):
+        tm = time(hour, minute, second, microsecond)
+        return tm.replace(tzinfo=timezone(timedelta(hours=offset)))
+
     def test_invalid_aware_times(self):
-        with self.assertRaises(ValueError):
-            parse_time('08:30-3')
-        with self.assertRaises(ValueError):
-            parse_time('08:30-03:')
-        with self.assertRaises(ValueError):
-            parse_time('08:30-03:00:')
-        with self.assertRaises(ValueError):
-            parse_time('08:30-03:00:00')
-        with self.assertRaises(ValueError):
-            parse_time('08:30-24:00')
-        with self.assertRaises(ValueError):
-            parse_time('08:30+24:00')
+        self.assertEqual(parse_time('08:30-3'), None)
+        self.assertEqual(parse_time('08:30-03:'), None)
+        self.assertEqual(parse_time('08:30-03:00:'), None)
+        self.assertEqual(parse_time('08:30-03:00:00'), None)
+        self.assertEqual(parse_time('08:30-24:00'), None)
+        self.assertEqual(parse_time('08:30+24:00'), None)
 
 
 class TimeZoneParserTestCase(TestCase):
@@ -91,18 +76,12 @@ class TimeZoneParserTestCase(TestCase):
         self.assertEqual(parse_timezone('+0000'), timedelta(0))
 
     def test_invalid_utc_timezones(self):
-        with self.assertRaises(ValueError):
-            parse_timezone('z')
-        with self.assertRaises(ValueError):
-            parse_timezone('+0')
-        with self.assertRaises(ValueError):
-            parse_timezone('+000')
-        with self.assertRaises(ValueError):
-            parse_timezone('+00000')
-        with self.assertRaises(ValueError):
-            parse_timezone('-00')
-        with self.assertRaises(ValueError):
-            parse_timezone('-0000')
+        self.assertEqual(parse_timezone('z'), None)
+        self.assertEqual(parse_timezone('+0'), None)
+        self.assertEqual(parse_timezone('+000'), None)
+        self.assertEqual(parse_timezone('+00000'), None)
+        self.assertEqual(parse_timezone('-00'), None)
+        self.assertEqual(parse_timezone('-0000'), None)
 
     def test_valid_timezones(self):
         self.assertEqual(parse_timezone('-03'), timedelta(hours=-3))
@@ -122,19 +101,12 @@ class TimeZoneParserTestCase(TestCase):
         self.assertEqual(parse_timezone('+2359'), timedelta(hours=23, minutes=59))
 
     def test_invalid_timezones(self):
-        with self.assertRaises(ValueError):
-            parse_timezone('-3')
-        with self.assertRaises(ValueError):
-            parse_timezone('-03:')
-        with self.assertRaises(ValueError):
-            parse_timezone('-03:00:')
-        with self.assertRaises(ValueError):
-            parse_timezone('-03:00:00')
-        with self.assertRaises(ValueError):
-            parse_timezone('-24:00')
-        with self.assertRaises(ValueError):
-            parse_timezone('+24:00')
-        with self.assertRaises(ValueError):
-            parse_timezone('-25:00')
-        with self.assertRaises(ValueError):
-            parse_timezone('+25:00')
+        self.assertEqual(parse_timezone(None), None)
+        self.assertEqual(parse_timezone('-3'), None)
+        self.assertEqual(parse_timezone('-03:'), None)
+        self.assertEqual(parse_timezone('-03:00:'), None)
+        self.assertEqual(parse_timezone('-03:00:00'), None)
+        self.assertEqual(parse_timezone('-24:00'), None)
+        self.assertEqual(parse_timezone('+24:00'), None)
+        self.assertEqual(parse_timezone('-25:00'), None)
+        self.assertEqual(parse_timezone('+25:00'), None)
