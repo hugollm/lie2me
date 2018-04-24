c3b47e88264a32c10c893368987c4d8a3df054d9
Hugo Mota <hugo.txt@gmail.com>
2018-04-23 23:37:23 -0300

    Replace lists for tuples in the boolean field validation

--- lie2me/fields/boolean.py
+++ lie2me/fields/boolean.py
@@ -9,8 +9,8 @@ class Boolean(Field):
 
     def validate(self, value):
         value = str(value).strip().lower()
-        if value in ['true', 'yes', '1', 'on']:
+        if value in ('true', 'yes', '1', 'on'):
             return True
-        if value in ['false', 'no', '0', 'off']:
+        if value in ('false', 'no', '0', 'off'):
             return False
         raise self.error('type')
